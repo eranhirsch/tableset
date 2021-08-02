3a2369bae0f9d636bb9dbe7730845966b72c603b
Eran Hirsch <eranhirsch@gmail.com>
2021-08-02 17:24:53 +0300

    with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Container } from "@material-ui/core";
 import { Template } from "./features/template/Template";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Container maxWidth="xs" component="main">
-      <Template />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Template />
+          </Route>
+        </Switch>
+      </Router>
     </Container>
   );
 }
