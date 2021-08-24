8c1ae9bff9f8a57e6b88442ae2c0b873c0ee228c
Eran Hirsch <eranhirsch@gmail.com>
2021-08-24 11:14:19 +0300

    initial panels for everything

--- src/common/short_name.ts
+++ src/common/short_name.ts
@@ -1,4 +1,4 @@
 export default function short_name(fullName: string): string {
   const [first, last] = fullName.split(" ");
-  return `${first[0]}${last[0]}`;
+  return last != null ? `${first[0]}${last[0]}` : first[0];
 }
--- src/features/instance/Instance.tsx
+++ src/features/instance/Instance.tsx
@@ -1,20 +1,114 @@
-import { List, ListItem } from "@material-ui/core";
+import {
+  Avatar,
+  AvatarGroup,
+  Badge,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@material-ui/core";
+import { EntityId } from "@reduxjs/toolkit";
 import { useAppSelector } from "../../app/hooks";
+import nullthrows from "../../common/err/nullthrows";
+import { useAppEntityIdSelectorEnforce } from "../../common/hooks/useAppEntityIdSelector";
+import PlayerColors from "../../common/PlayerColors";
+import short_name from "../../common/short_name";
 import { SetupStepName } from "../../games/concordia/ConcordiaGame";
-import { selectors } from "./instanceSlice";
+import { stepLabel } from "../../games/concordia/content";
+import { selectors as playersSelectors } from "../players/playersSlice";
+import { selectors as instanceSelectors } from "./instanceSlice";
+
+function PlayOrderPanel({ playOrder }: { playOrder: ReadonlyArray<EntityId> }) {
+  const players = useAppSelector(playersSelectors.selectEntities);
+
+  return (
+    <AvatarGroup>
+      {playOrder.map((playerId) => (
+        <Avatar key={playerId}>
+          {short_name(nullthrows(players[playerId]).name)}
+        </Avatar>
+      ))}
+    </AvatarGroup>
+  );
+}
+
+function PlayerColorsPanel({ playerColor }: { playerColor: PlayerColors }) {
+  const players = useAppSelector(playersSelectors.selectEntities);
+
+  return (
+    <Stack direction="row" spacing={1}>
+      {Object.entries(playerColor).map(([playerId, color]) => (
+        <Badge
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          overlap="circular"
+          invisible={false}
+          color={color}
+        >
+          <Avatar key={playerId}>
+            {short_name(nullthrows(players[playerId]).name)}
+          </Avatar>
+        </Badge>
+      ))}
+    </Stack>
+  );
+}
+
+function FirstPlayerPanel({ playerId }: { playerId: EntityId }) {
+  const player = useAppEntityIdSelectorEnforce(playersSelectors, playerId);
+
+  return <Avatar>{short_name(player.name)}</Avatar>;
+}
+
+function InstanceItemContent({ stepId }: { stepId: SetupStepName }) {
+  const step = useAppEntityIdSelectorEnforce(instanceSelectors, stepId);
+
+  switch (step.id) {
+    case "playOrder":
+      return <PlayOrderPanel playOrder={step.value} />;
+
+    case "playerColors":
+      return <PlayerColorsPanel playerColor={step.value} />;
+
+    case "firstPlayer":
+      return <FirstPlayerPanel playerId={step.value} />;
+
+    default:
+      return <Typography variant="h4">{step.value}</Typography>;
+  }
+}
 
 function InstanceItem({ stepId }: { stepId: SetupStepName }) {
-  return <ListItem>Hello World</ListItem>;
+  const label = stepLabel(stepId);
+  return (
+    <Card component="li">
+      <CardHeader avatar={<Avatar>{short_name(label)}</Avatar>} title={label} />
+      <CardContent
+        sx={{
+          justifyContent: "center",
+          display: "flex",
+          paddingX: 2,
+        }}
+      >
+        <InstanceItemContent stepId={stepId} />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default function Instance() {
-  const stepIds = useAppSelector(selectors.selectIds);
+  const stepIds = useAppSelector(instanceSelectors.selectIds);
 
   return (
-    <List component="ol">
+    <Stack
+      component="ol"
+      sx={{ listStyle: "none", paddingInlineStart: 0 }}
+      direction="column"
+      spacing={1}
+    >
       {stepIds.map((stepId) => (
-        <InstanceItem stepId={stepId as SetupStepName} />
+        <InstanceItem key={stepId} stepId={stepId as SetupStepName} />
       ))}
-    </List>
+    </Stack>
   );
 }
