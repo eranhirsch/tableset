b029e4182a8408f2aa469c16b30969d6095ce716
Eran Hirsch <eranhirsch@gmail.com>
2021-08-25 14:40:53 +0300

    move files

---
+++ src/app/App.tsx
@@ -0,0 +1,53 @@
+import { Container, ThemeProvider } from "@material-ui/core";
+import Template from "./features/template/Template";
+import { Switch, Route, Redirect } from "react-router-dom";
+import themeWithGameColors from "./core/themeWithGameColors";
+import Players from "./features/players/Players";
+import { useEffect, useState } from "react";
+import { useAppDispatch } from "./app/hooks";
+import playersSlice from "./features/players/playersSlice";
+import Instance from "./features/instance/Instance";
+import { TableSetAppBar } from "./app/TableSetAppBar";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!isInitialized) {
+      // TODO: Remove this when we can load the previous players from somewhere
+      ["Eran Hirsch", "Adam Maoz", "Amit Cwajghaft"].forEach((name) =>
+        dispatch(playersSlice.actions.added(name))
+      );
+      setIsInitialized(true);
+    }
+  }, [dispatch, isInitialized]);
+
+  return (
+    <ThemeProvider theme={themeWithGameColors}>
+      <TableSetAppBar />
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{ height: "100vh", position: "relative" }}
+      >
+        <Switch>
+          <Route path="/template">
+            <Template />
+          </Route>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/instance">
+            <Instance />
+          </Route>
+          <Route path="/">
+            <Redirect to="/template" />
+          </Route>
+        </Switch>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/app/TableSetAppBar.tsx
@@ -0,0 +1,44 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { Switch, Route, Link } from "react-router-dom";
+import PeopleIcon from "@material-ui/icons/People";
+import TuneIcon from "@material-ui/icons/Tune";
+import ClearIcon from "@material-ui/icons/Clear";
+
+export function TableSetAppBar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Switch>
+            <Route path="/template">Template</Route>
+            <Route path="/players">Players</Route>
+            <Route path="/instance">Instance</Route>
+            <Route path="/">MISSING TITLE</Route>
+          </Switch>
+        </Typography>
+        <Switch>
+          <Route path="/template">
+            <IconButton component={Link} color="inherit" to="/players">
+              <PeopleIcon />
+            </IconButton>
+          </Route>
+          <Route path="/players">
+            <IconButton component={Link} color="inherit" to="/template">
+              <TuneIcon />
+            </IconButton>
+          </Route>
+          <Route path="/instance/:stepId">
+            <IconButton component={Link} color="inherit" to="/instance">
+              <ClearIcon />
+            </IconButton>
+          </Route>
+          <Route path="/instance">
+            <IconButton component={Link} color="inherit" to="/template">
+              <ClearIcon />
+            </IconButton>
+          </Route>
+        </Switch>
+      </Toolbar>
+    </AppBar>
+  );
+}
