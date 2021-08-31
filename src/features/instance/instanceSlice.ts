626ac26e1f8f9b6157c2d0e3aef1a34b89f4672b
Eran Hirsch <eranhirsch@gmail.com>
2021-08-31 17:43:04 +0300

    proper coding

--- src/features/instance/instanceSlice.ts
+++ src/features/instance/instanceSlice.ts
@@ -1,9 +1,10 @@
 import { createEntityAdapter, createSlice, Dictionary } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import invariant_violation from "../../common/err/invariant_violation";
-import filter_nulls from "../../common/lib_utils/filter_nulls";
+import nullthrows from "../../common/err/nullthrows";
 import { Strategy } from "../../core/Strategy";
 import Game, { StepId } from "../../games/IGame";
+import { InstanceContext } from "../../games/IGameStep";
 import { PlayerId } from "../players/playersSlice";
 import { TemplateElement } from "../template/templateSlice";
 
@@ -21,63 +22,27 @@ export const instanceSlice = createSlice({
   initialState: instanceAdapter.getInitialState(),
   reducers: {
     created: {
-      prepare(
+      prepare: (
         game: Game,
         template: Dictionary<TemplateElement>,
         playerIds: readonly PlayerId[]
-      ) {
-        const payload: SetupStep[] = [];
-        for (const element of filter_nulls(Object.values(template))) {
-          let step: SetupStep;
-
-          switch (element.strategy) {
-            case Strategy.RANDOM:
-              step = {
-                id: element.id,
-                value: game.at(element.id)!.resolveRandom!({
-                  instance: payload,
-                  playerIds,
-                }),
-              };
-              break;
-
-            case Strategy.DEFAULT:
-              switch (element.id) {
-                case "playOrder":
-                case "playerColors":
-                case "firstPlayer":
-                  invariant_violation(`No default setting for ${element.id}`);
-                  break;
-
-                default:
-                  step = {
-                    id: element.id,
-                    value: game.at(element.id)!.resolveDefault!({
-                      instance: payload,
-                      playerIds,
-                    }),
-                  };
-              }
-              break;
-
-            case Strategy.FIXED:
-              step = {
-                id: element.id,
-                value: element.value as any,
-              };
-              break;
-
-            default:
-              invariant_violation(
-                `Unimplemented strategy type ${element.strategy} at step ${element.id}`
-              );
+      ) => ({
+        payload: game.order.reduce((ongoing, stepId) => {
+          const element = template[stepId];
+          if (element == null) {
+            return ongoing;
           }
 
-          payload.push(step);
-        }
-
-        return { payload };
-      },
+          const setupStep = {
+            id: stepId,
+            value: elementResolver(element, game, {
+              instance: ongoing,
+              playerIds,
+            }),
+          };
+          return ongoing.concat(setupStep);
+        }, [] as readonly SetupStep[]),
+      }),
 
       reducer: instanceAdapter.setAll,
     },
@@ -89,3 +54,40 @@ export const selectors = instanceAdapter.getSelectors<RootState>(
 );
 
 export default instanceSlice;
+
+function elementResolver(
+  element: TemplateElement,
+  game: Game,
+  context: InstanceContext
+): any {
+  const gameStep = nullthrows(
+    game.at(element.id),
+    `Element ${JSON.stringify(
+      element
+    )} does not have a corresponding step in the game`
+  );
+
+  switch (element.strategy) {
+    case Strategy.RANDOM:
+      return nullthrows(
+        gameStep.resolveRandom,
+        `Element ${JSON.stringify(element)} does not have a random resolver`
+      )(context);
+
+    case Strategy.DEFAULT:
+      return nullthrows(
+        gameStep.resolveDefault,
+        `Element ${JSON.stringify(element)} does not have a default resolver`
+      )(context);
+
+    case Strategy.FIXED:
+      // Just copy the value
+      return element.value as any;
+  }
+
+  invariant_violation(
+    `Element ${JSON.stringify(
+      element
+    )} is using a strategy that isn't supported`
+  );
+}
