7c19cb006fe9b61277eca625bd51aa6539553641
Eran Hirsch <eranhirsch@gmail.com>
2021-09-02 18:51:02 +0300

    improve starting colonist

--- src/games/concordia/ux/StartingColonists.tsx
+++ src/games/concordia/ux/StartingColonists.tsx
@@ -1,25 +1,38 @@
+import React from "react";
 import { MapId, MAPS } from "../steps/mapStep";
+import GrammaticalList from "../../core/ux/GrammaticalList";
+import { Typography } from "@material-ui/core";
 
 export default function StartingColonists({ mapId }: { mapId: MapId }) {
   const { startingColonists } = MAPS[mapId];
 
-  if (startingColonists[0].placement === startingColonists[1].placement) {
-    const phrases = startingColonists.map(
-      (colonistLocation) => `1 ${colonistLocation.type} colonist`
-    );
+  if (startingColonists[0].placement !== startingColonists[1].placement) {
+    const phrases = startingColonists.map((colonist, idx) => (
+      <React.Fragment key={`colonist_${idx}`}>
+        1 <strong>{colonist.type}</strong> colonist from their storehouse and
+        places them in
+        <strong>{colonist.placement}</strong>.
+      </React.Fragment>
+    ));
+
     return (
-      <div>{`Each player takes ${phrases.join(
-        " and "
-      )} from their storehouse and places them in ${
-        startingColonists[0].placement
-      }`}</div>
+      <Typography variant="body2">
+        Each player takes <GrammaticalList>{phrases}</GrammaticalList>
+      </Typography>
     );
   }
 
-  const phrases = startingColonists.map(
-    (colonist) =>
-      `1 ${colonist.type} colonist from their storehouse and places them in ${colonist.placement}`
-  );
+  const phrases = startingColonists.map((colonistLocation, idx) => (
+    <React.Fragment key={`colonist_${idx}`}>
+      1 <strong>{colonistLocation.type}</strong> colonist
+    </React.Fragment>
+  ));
 
-  return <div>{`Each player takes ${phrases.join(", and ")}`}</div>;
+  return (
+    <Typography variant="body2">
+      Each player takes <GrammaticalList>{phrases}</GrammaticalList> from their
+      storehouse and places them in{" "}
+      <strong>{startingColonists[0].placement}</strong>.
+    </Typography>
+  );
 }
---
+++ src/games/core/ux/GrammaticalList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function GrammaticalList({
+  children,
+}: {
+  children: JSX.Element[];
+}) {
+  if (children.length === 1) {
+    return children[0];
+  }
+
+  if (children.length === 2) {
+    return (
+      <>
+        {children[0]} and {children[1]}
+      </>
+    );
+  }
+
+  return (
+    <>
+      {children.slice(0, children.length - 1).map((child, idx) => (
+        <React.Fragment key={`grammatical_${idx}`}>{child}, </React.Fragment>
+      ))}{" "}
+      and {children[children.length - 1]}
+    </>
+  );
+}
