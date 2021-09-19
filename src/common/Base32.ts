dd41c639e55afa06c3ac7aaf5696466f1f72ac0f
Eran Hirsch <eranhirsch@gmail.com>
2021-09-19 13:44:08 +0300

    restructure

--- src/common/Base32.ts
+++ src/common/Base32.ts
@@ -1,18 +1,24 @@
-export default class Base32 {
-  public static encode(x: number): string {
-    return x
-      .toString(32)
-      .toUpperCase()
-      .replace("0", "W")
-      .replace("1", "X")
-      .replace("O", "Y")
-      .replace("I", "Z");
-  }
+/**
+ * These characters are too similar in most fonts, making it hard to tell what
+ * character to use when copying the string manually. We change them in the
+ * encoded string to use the remaining 4 characters that aren't used (because we
+ * use 10 digits + 22 characters === 32)
+ */
+const REPLACEMENTS = [
+  ["0", "W"],
+  ["1", "X"],
+  ["O", "Y"],
+  ["I", "Z"],
+] as const;
 
-  public static decode(x: string): number {
-    return Number.parseInt(
-      x.replace("Z", "I").replace("Y", "O").replace("X", "1").replace("W", "0"),
-      32
-    );
-  }
-}
+export const encode = (x: number): string =>
+  REPLACEMENTS.reduce(
+    (out, [from, to]) => out.replace(from, to),
+    x.toString(32).toUpperCase()
+  );
+
+export const decode = (x: string): number =>
+  Number.parseInt(
+    REPLACEMENTS.reduce((out, [to, from]) => out.replace(from, to), x),
+    32
+  );
