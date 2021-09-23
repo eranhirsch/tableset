50fe7e3619a58d1422babb7d6efceda9822413cf
Eran Hirsch <eranhirsch@gmail.com>
2021-09-24 00:29:43 +0300

    player color stl

--- src/games/global/ux/PlayerColorsFixedTemplateLabel.tsx
+++ src/games/global/ux/PlayerColorsFixedTemplateLabel.tsx
@@ -1,4 +1,6 @@
 import { Chip } from "@mui/material";
+import { Vec } from "common";
+import { PlayerId } from "model/Player";
 import React from "react";
 import GrammaticalList from "../../core/ux/GrammaticalList";
 import { PlayerColors } from "../steps/createPlayerColorsStep";
@@ -12,12 +14,16 @@ export default function PlayersColorsFixedTemplateLabel({
   return (
     <GrammaticalList>
       {React.Children.toArray(
-        Object.entries(value).map(([playerId, color]) => (
+        Vec.map_with_key(value, (playerId, color) => (
           <Chip
             component="span"
             size="small"
             color={color}
-            label={<PlayerShortName playerId={playerId} />}
+            // TODO: Something about the typing of Vec.map_with_key isn't
+            // inferring the keys of the Record properly, sending a number type
+            // here. We need to fix the typing there and then remove the `as`
+            // here
+            label={<PlayerShortName playerId={playerId as PlayerId} />}
           />
         ))
       )}
