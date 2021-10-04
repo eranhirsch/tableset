df2935a563dd526f4c8eff514ed1fca6a32475a1
Eran Hirsch <eranhirsch@gmail.com>
2021-10-04 12:06:42 +0300

    cleaner

--- src/games/concordia/utils/CityResourcesEncoder.ts
+++ src/games/concordia/utils/CityResourcesEncoder.ts
@@ -15,16 +15,14 @@ import { Resource, RESOURCE_COST } from "./resource";
 const HASH_SEPARATOR = "-";
 
 type ZoneResourceTiles = Readonly<Record<Resource, number>>;
+export const CITY_TILES = Object.freeze({
+  A: Object.freeze({ bricks: 2, food: 2, tools: 1, wine: 1, cloth: 1 }),
+  B: Object.freeze({ bricks: 2, food: 3, tools: 1, wine: 1, cloth: 1 }),
+  C: Object.freeze({ bricks: 3, food: 2, tools: 2, wine: 2, cloth: 1 }),
+  D: Object.freeze({ bricks: 1, food: 1, tools: 1, wine: 1, cloth: 1 }),
+} as Record<Zone, ZoneResourceTiles>);
 
-export const CITY_TILES: Readonly<Record<Zone, ZoneResourceTiles>> =
-  Object.freeze({
-    A: Object.freeze({ bricks: 2, food: 2, tools: 1, wine: 1, cloth: 1 }),
-    B: Object.freeze({ bricks: 2, food: 3, tools: 1, wine: 1, cloth: 1 }),
-    C: Object.freeze({ bricks: 3, food: 2, tools: 2, wine: 2, cloth: 1 }),
-    D: Object.freeze({ bricks: 1, food: 1, tools: 1, wine: 1, cloth: 1 }),
-  });
-
-type CityResources = Readonly<{ [cityName: string]: Resource }>;
+type CityResources = Readonly<Record<string /* cityName */, Resource>>;
 type ProvinceCityResources = Readonly<
   Record<string /* provinceName */, CityResources>
 >;
@@ -60,37 +58,34 @@ function decodeCityResources(
   mapId: MapId,
   hash: string
 ): ProvinceCityResources {
-  return Dict.flatten(
-    Vec.map(
-      Vec.map(
-        Vec.zip(
-          Vec.values(
-            Shape.inner_join(
-              MAPS[mapId].provinces,
-              Dict.map(CITY_TILES, MathUtils.permutations_lazy_array)
-            )
-          ),
-          hash.split(HASH_SEPARATOR).map(Num.decode_base32)
-        ),
-        ([[provinces, permutations], permutationIndex]) =>
-          tuple(
-            provinces,
-            nullthrows(
-              permutations.at(permutationIndex),
-              `Index ${permutationIndex} is out of bounds for permutations ${permutations}`
-            )
-          )
-      ),
-      ([provinces, resources]) =>
-        Dict.map(provinces, (cities) => {
-          const cityResources = Vec.zip(cities, resources);
-          invariant(
-            cityResources.length === cities.length,
-            `Number of resources ${cityResources.length} didn't match number of cities ${cities.length}`
-          );
-          resources = resources.slice(cities.length);
-          return Dict.from_entries(cityResources);
-        })
-    )
+  const allPerms = Dict.map(CITY_TILES, MathUtils.permutations_lazy_array);
+  const zonePerms = Shape.inner_join(MAPS[mapId].provinces, allPerms);
+  const relevantPerms = Vec.values(zonePerms);
+  const withHashIndices = Vec.zip(
+    relevantPerms,
+    hash.split(HASH_SEPARATOR).map(Num.decode_base32)
   );
+  const zoneResources = Vec.map(
+    withHashIndices,
+    ([[provinces, permutations], permutationIndex]) =>
+      tuple(
+        provinces,
+        nullthrows(
+          permutations.at(permutationIndex),
+          `Index ${permutationIndex} is out of bounds for permutations ${permutations}`
+        )
+      )
+  );
+  const provincesResources = Vec.map(zoneResources, ([provinces, resources]) =>
+    Dict.map(provinces, (cities) => {
+      const cityResources = Vec.zip(cities, resources);
+      invariant(
+        cityResources.length === cities.length,
+        `Number of resources ${cityResources.length} didn't match number of cities ${cities.length}`
+      );
+      resources = resources.slice(cities.length);
+      return Dict.from_entries(cityResources);
+    })
+  );
+  return Dict.flatten(provincesResources);
 }
