fb960dfd4b69bf1c3ab2aa508e27cf40d7fa88d6
Eran Hirsch <eranhirsch@gmail.com>
2021-10-07 18:44:28 +0300

    summary for salt

--- src/games/concordia/steps/salsaVariantStep.tsx
+++ src/games/concordia/steps/salsaVariantStep.tsx
@@ -1,6 +1,10 @@
+import { InstanceStepLink } from "features/instance/InstanceStepLink";
 import createProductDependencyMetaStep from "games/core/steps/createProductDependencyMetaStep";
 import { createVariant } from "games/core/steps/createVariant";
+import { BlockWithFootnotes } from "games/core/ux/BlockWithFootnotes";
 import { ConcordiaProductId } from "../concordiaGame";
+import { RESOURCE_NAME } from "../utils/resource";
+import RomanTitle from "../ux/RomanTitle";
 
 export default createVariant({
   id: "salsa",
@@ -10,5 +14,29 @@ export default createVariant({
 });
 
 function InstanceVariableComponent(): JSX.Element {
-  return <div>Hello World!</div>;
+  return (
+    <BlockWithFootnotes
+      footnotes={[
+        <InstanceStepLink stepId="cityTiles" />,
+        <InstanceStepLink stepId="resourcePiles" />,
+        <>
+          e.g. paying for houses via an <RomanTitle>Architect</RomanTitle>,
+          buying cards via a <RomanTitle>Senator</RomanTitle> or a{" "}
+          <RomanTitle>Consul</RomanTitle>, buying colonists with a{" "}
+          <RomanTitle>Colonist</RomanTitle> or a{" "}
+          <RomanTitle>Tribune</RomanTitle>, or <em>selling</em> resources via a{" "}
+          <RomanTitle>Mercator</RomanTitle>.
+        </>,
+      ]}
+    >
+      {(Footnote) => (
+        <>
+          Some cities now produce
+          <Footnote index={1} /> <strong>{RESOURCE_NAME.salt}</strong>
+          <Footnote index={2} /> which could be used as any other resource
+          <Footnote index={3} />.
+        </>
+      )}
+    </BlockWithFootnotes>
+  );
 }
