d64ec10b6aa62a7595fbe2a9c23b27c17e4bee45
Eran Hirsch <eranhirsch@gmail.com>
2021-10-12 17:59:25 +0300

    play order config panel

--- src/games/global/steps/playOrderStep.tsx
+++ src/games/global/steps/playOrderStep.tsx
@@ -1,17 +1,34 @@
-import { AvatarGroup, Box, Typography } from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import {
+  Avatar,
+  AvatarGroup,
+  Badge,
+  Box,
+  FormControlLabel,
+  Stack,
+  Switch,
+  Typography
+} from "@mui/material";
 import { Vec } from "common";
+import { PlayerNameShortAbbreviation } from "features/players/PlayerNameShortAbbreviation";
 import { ConfigPanelProps } from "features/template/Templatable";
 import { templateValue } from "features/template/templateSlice";
 import { playersMetaStep } from "games/core/steps/createPlayersDependencyMetaStep";
 import { Query } from "games/core/steps/Query";
-import React from "react";
+import React, { useCallback, useMemo } from "react";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult
+} from "react-beautiful-dnd";
 import { useAppSelector } from "../../../app/hooks";
 import { PlayerAvatar } from "../../../features/players/PlayerAvatar";
 import { firstPlayerIdSelector } from "../../../features/players/playersSlice";
 import { PlayerId } from "../../../model/Player";
 import {
   createRandomGameStep,
-  VariableStepInstanceComponentProps,
+  VariableStepInstanceComponentProps
 } from "../../core/steps/createRandomGameStep";
 import { BlockWithFootnotes } from "../../core/ux/BlockWithFootnotes";
 
@@ -29,9 +46,6 @@ export default createRandomGameStep({
   InstanceVariableComponent,
   InstanceManualComponent,
 
-  // TODO: Move to the resolver
-  // random: (playerIds) =>
-
   isTemplatable: (players) =>
     players.count({
       // It's meaningless to talk about order with less than 3 players
@@ -41,7 +55,7 @@ export default createRandomGameStep({
   resolve: (config: TemplateConfig, playerIds) =>
     "fixed" in config ? config.fixed : Vec.shuffle(playerIds!.slice(1)),
 
-  initialConfig: (players) => ({ fixed: players.resolve().slice(1) }),
+  initialConfig: (players) => ({ fixed: defaultPlayOrder(players) }),
 
   refresh: (current, players) =>
     "fixed" in current
@@ -51,12 +65,158 @@ export default createRandomGameStep({
   ConfigPanel,
 });
 
+const defaultPlayOrder = (
+  players: Query<readonly PlayerId[]>
+): readonly PlayerId[] => players.resolve().slice(1);
+
 function ConfigPanel({
   config,
-  queries,
+  queries: [players],
   onChange,
 }: ConfigPanelProps<TemplateConfig, readonly PlayerId[]>): JSX.Element {
-  return <div>Hello World</div>;
+  const initialFixed = useMemo(() => defaultPlayOrder(players), [players]);
+  return (
+    <Stack direction="column" spacing={1}>
+      <FixedSelector
+        currentOrder={
+          config != null && "fixed" in config ? config.fixed : initialFixed
+        }
+        disabled={config == null || "random" in config}
+        onChange={(newOrder) => onChange({ fixed: newOrder })}
+      />
+      <FormControlLabel
+        sx={{ alignSelf: "center" }}
+        control={
+          <Switch
+            checked={config != null && "random" in config}
+            onChange={(_, checked) =>
+              onChange(checked ? { random: true } : { fixed: initialFixed })
+            }
+          />
+        }
+        label="Random"
+      />
+    </Stack>
+  );
+}
+
+function FixedSelector({
+  currentOrder,
+  onChange,
+  disabled,
+}: {
+  currentOrder: readonly PlayerId[];
+  onChange(newOrder: readonly PlayerId[]): void;
+  disabled: boolean;
+}): JSX.Element {
+  const onDragEnd = useCallback(
+    ({ reason, source, destination }: DropResult) => {
+      if (reason === "CANCEL") {
+        return;
+      }
+
+      if (destination == null) {
+        // Dropped out of the droppable
+        return;
+      }
+
+      const value = moveItem(currentOrder, source.index, destination.index);
+      onChange(value);
+    },
+    [currentOrder, onChange]
+  );
+
+  return (
+    <Stack
+      sx={{ opacity: disabled ? 0.5 : 1.0 }}
+      alignItems="center"
+      direction="column"
+      spacing={1}
+    >
+      <Stack direction="row" spacing={1}>
+        <FirstAvatar />
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="order" direction="horizontal">
+            {(provided) => (
+              <Stack
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                component="ol"
+                direction="row"
+                sx={{ padding: 0 }}
+                spacing={1}
+              >
+                {currentOrder.map((playerId, idx) => (
+                  <DraggablePlayer
+                    isDragDisabled={disabled}
+                    key={playerId}
+                    playerId={playerId}
+                    index={idx}
+                  />
+                ))}
+                {provided.placeholder}
+              </Stack>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Stack>
+      <Typography variant="caption">{"In clockwise order"}</Typography>
+    </Stack>
+  );
+}
+
+function moveItem<T>(
+  items: readonly T[],
+  itemIdx: number,
+  targetIdx: number
+): T[] {
+  const clone = items.slice();
+  const [item] = clone.splice(itemIdx, 1);
+  clone.splice(targetIdx, 0, item);
+  return clone;
+}
+
+function DraggablePlayer({
+  playerId,
+  index,
+  isDragDisabled,
+}: {
+  playerId: PlayerId;
+  index: number;
+  badgeContent?: string;
+  isDragDisabled: boolean;
+}) {
+  return (
+    <Draggable
+      isDragDisabled={isDragDisabled}
+      draggableId={`${playerId}`}
+      index={index}
+    >
+      {(provided) => (
+        <Avatar
+          component="li"
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+        >
+          <PlayerNameShortAbbreviation playerId={playerId} />
+        </Avatar>
+      )}
+    </Draggable>
+  );
+}
+
+function FirstAvatar() {
+  const playerId = useAppSelector(firstPlayerIdSelector);
+  return (
+    <Badge
+      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      overlap="circular"
+      badgeContent={<LockIcon color="primary" fontSize="small" />}
+    >
+      <PlayerAvatar playerId={playerId} />
+    </Badge>
+  );
 }
 
 function InstanceVariableComponent({
@@ -119,116 +279,6 @@ function InstanceManualComponent(): JSX.Element {
 //         ))
 //       )}
 //     </>
-//   );
-// }
-
-// function Selector({
-//   current: order,
-// }: {
-//   current: readonly PlayerId[];
-// }): JSX.Element {
-//   const onDragEnd = useCallback(
-//     ({ reason, source, destination }: DropResult) => {
-//       if (reason === "CANCEL") {
-//         return;
-//       }
-
-//       if (destination == null) {
-//         // Dropped out of the droppable
-//         return;
-//       }
-
-//       const value = moveItem(order, source.index, destination.index);
-//       console.log(value); // TODO: just to make the variable used
-//       // TODO: Modernize to ConfigPanel
-//       // dispatch(
-//       //   templateActions.constantValueChanged({
-//       //     id: "playOrder",
-//       //     value,
-//       //   })
-//       // );
-//     },
-//     [order]
-//   );
-
-//   return (
-//     <Stack alignItems="center" direction="column" spacing={1}>
-//       <Stack direction="row" spacing={1}>
-//         <FirstAvatar />
-//         <DragDropContext onDragEnd={onDragEnd}>
-//           <Droppable droppableId="order" direction="horizontal">
-//             {(provided) => (
-//               <Stack
-//                 ref={provided.innerRef}
-//                 {...provided.droppableProps}
-//                 component="ol"
-//                 direction="row"
-//                 sx={{ padding: 0 }}
-//                 spacing={1}
-//               >
-//                 {order.map((playerId, idx) => (
-//                   <DraggablePlayer
-//                     key={playerId}
-//                     playerId={playerId}
-//                     index={idx}
-//                   />
-//                 ))}
-//                 {provided.placeholder}
-//               </Stack>
-//             )}
-//           </Droppable>
-//         </DragDropContext>
-//       </Stack>
-//       <Typography variant="caption">{"In clockwise order"}</Typography>
-//     </Stack>
-//   );
-// }
-
-// function moveItem<T>(
-//   items: readonly T[],
-//   itemIdx: number,
-//   targetIdx: number
-// ): T[] {
-//   const clone = items.slice();
-//   const [item] = clone.splice(itemIdx, 1);
-//   clone.splice(targetIdx, 0, item);
-//   return clone;
-// }
-
-// function DraggablePlayer({
-//   playerId,
-//   index,
-// }: {
-//   playerId: PlayerId;
-//   index: number;
-//   badgeContent?: string;
-// }) {
-//   return (
-//     <Draggable draggableId={`${playerId}`} index={index}>
-//       {(provided) => (
-//         <Avatar
-//           component="li"
-//           ref={provided.innerRef}
-//           {...provided.dragHandleProps}
-//           {...provided.draggableProps}
-//         >
-//           <PlayerNameShortAbbreviation playerId={playerId} />
-//         </Avatar>
-//       )}
-//     </Draggable>
-//   );
-// }
-
-// function FirstAvatar() {
-//   const playerId = useAppSelector(firstPlayerIdSelector);
-//   return (
-//     <Badge
-//       anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-//       overlap="circular"
-//       badgeContent={<LockIcon color="primary" fontSize="small" />}
-//     >
-//       <PlayerAvatar playerId={playerId} />
-//     </Badge>
 //   );
 // }
 
