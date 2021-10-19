d33c8c298a9b5eee5fdbb913230d8002d7d337cc
Eran Hirsch <eranhirsch@gmail.com>
2021-10-19 09:12:17 +0300

    sort expansions and bases

--- src/features/expansions/Expansions.tsx
+++ src/features/expansions/Expansions.tsx
@@ -1,6 +1,6 @@
 import { Checkbox, List, ListItem, ListItemText } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "app/hooks";
-import { Vec } from "common";
+import { Dict, Vec } from "common";
 import { gameSelector } from "features/game/gameSlice";
 import { ProductId } from "model/Game";
 import React from "react";
@@ -31,14 +31,15 @@ export function ExpansionListItem({
 }
 
 export function Expansions(): JSX.Element {
-  const game = useAppSelector(gameSelector);
+  const { products } = useAppSelector(gameSelector);
 
   return (
     <List sx={{ width: "100%" }}>
       {React.Children.toArray(
-        Vec.map_with_key(game.products, (productId) => (
-          <ExpansionListItem productId={productId} />
-        ))
+        Vec.map_with_key(
+          Dict.sort_by(products, ({ isBase }) => (isBase ? 0 : 1)),
+          (productId) => <ExpansionListItem productId={productId} />
+        )
       )}
     </List>
   );
