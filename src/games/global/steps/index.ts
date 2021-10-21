662d0cdc9247d9db00abef36abeb5ab26265cb65
Eran Hirsch <eranhirsch@gmail.com>
2021-10-21 11:08:35 +0300

    i broke index.ts

--- src/games/global/steps/index.ts
+++ src/games/global/steps/index.ts
@@ -1,3 +1,2 @@
-export { default as createPlayerColorsStep } from "./DEPRECATED_createPlayerColorsStep";
 export { default as firstPlayerStep } from "./firstPlayerStep";
 export { default as playOrderStep } from "./playOrderStep";
