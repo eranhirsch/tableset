2f5c23d6c2fb1c6b9509eb790c51c3c2bd75f0c4
Eran Hirsch <eranhirsch@gmail.com>
2021-10-24 20:33:23 +0300

    prevent venus scoring from generating a value if teamplay is on

--- src/games/concordia/steps/venusScoringVariant.tsx
+++ src/games/concordia/steps/venusScoringVariant.tsx
@@ -1,18 +1,65 @@
-import { Typography } from "@mui/material";
-import { createVariant } from "games/core/steps/createVariant";
+import { Box, Typography } from "@mui/material";
+import { Random } from "common";
+import { ConfigPanelProps } from "features/template/Templatable";
+import { templateValue } from "features/template/templateSlice";
+import { createRandomGameStep } from "games/core/steps/createRandomGameStep";
+import { PercentSlider } from "games/core/ux/PercentSlider";
+import { ConcordiaProductId } from "../ConcordiaProductId";
 import RomanTitle from "../ux/RomanTitle";
 import productsMetaStep from "./productsMetaStep";
 import teamPlayVariant from "./teamPlayVariant";
 
-export default createVariant({
-  id: "venusScoring",
-  name: "Venus Scoring",
+type TemplateConfig = { percent: number };
+
+export default createRandomGameStep<
+  boolean,
+  TemplateConfig,
+  readonly ConcordiaProductId[],
+  boolean
+>({
+  id: "variant_venusScoring",
+  labelOverride: "Variant: Venus Scoring",
   dependencies: [productsMetaStep, teamPlayVariant],
   isTemplatable: (products, isTeamPlay) =>
     products.willContainAny(["venus", "venusBase"]) &&
     isTeamPlay.canResolveTo(false),
   InstanceVariableComponent,
+  initialConfig: (): Readonly<TemplateConfig> => ({ percent: 100 }),
+  resolve: ({ percent }, _, teamPlay): true | null =>
+    !teamPlay && Random.coin_flip(percent / 100) ? true : null,
+  refresh: () => templateValue("unchanged"),
+  skip: (value) => value == null,
+  ConfigPanel,
+  ConfigPanelTLDR,
+  disabledTLDROverride: "Never",
+  canResolveTo: (value, config) =>
+    value
+      ? config != null && config.percent > 0
+      : config == null || config.percent < 100,
 });
+
+function ConfigPanel({
+  config: { percent },
+  onChange,
+}: ConfigPanelProps<TemplateConfig>): JSX.Element {
+  return (
+    <Box textAlign="center">
+      <PercentSlider
+        percent={percent}
+        onChange={(percent) => onChange({ percent })}
+        preventZero
+      />
+    </Box>
+  );
+}
+
+function ConfigPanelTLDR({
+  config: { percent },
+}: {
+  config: TemplateConfig;
+}): JSX.Element {
+  return percent === 100 ? <>Always</> : <>{percent}% chance</>;
+}
 
 function InstanceVariableComponent(): JSX.Element {
   return (
