499e885dc764ee681331657ebf985f40c6bdc0ba
Eran Hirsch <eranhirsch@gmail.com>
2021-10-31 18:19:28 +0200

    better buttons

--- src/app/ux/AppBar.tsx
+++ src/app/ux/AppBar.tsx
@@ -1,12 +1,17 @@
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import ClearIcon from "@mui/icons-material/Clear";
+import HomeIcon from "@mui/icons-material/Home";
 import PeopleIcon from "@mui/icons-material/People";
 import TuneIcon from "@mui/icons-material/Tune";
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { useAppSelector } from "app/hooks";
+import { gameSelector } from "features/game/gameSlice";
 import { Link, Route, Switch } from "react-router-dom";
 import { OverflowMenu } from "./OverflowMenu";
 
 export function TableSetAppBar(): JSX.Element {
+  const game = useAppSelector(gameSelector);
+
   const templateButton = (
     <IconButton component={Link} color="inherit" to="/template">
       <TuneIcon />
@@ -20,6 +25,11 @@ export function TableSetAppBar(): JSX.Element {
   const productsButton = (
     <IconButton component={Link} color="inherit" to="/collection">
       <AddBoxIcon />
+    </IconButton>
+  );
+  const gameHomeButton = (
+    <IconButton component={Link} color="inherit" to={`/${game.id}`}>
+      <HomeIcon />
     </IconButton>
   );
 
@@ -37,23 +47,16 @@ export function TableSetAppBar(): JSX.Element {
           </Switch>
         </Typography>
         <Switch>
-          <Route path="/template">
-            {playersButton}
-            {productsButton}
-          </Route>
-          <Route path="/players">
-            {templateButton}
-            {productsButton}
-          </Route>
-          <Route path="/products">
-            {templateButton}
-            {playersButton}
-          </Route>
+          <Route path="/template">{playersButton}</Route>
+          <Route path="/players">{templateButton}</Route>
+          <Route path="/collection">{gameHomeButton}</Route>
           <Route path="/instance">
             <IconButton component={Link} color="inherit" to="/template">
               <ClearIcon />
             </IconButton>
           </Route>
+          <Route path="/games" />
+          <Route path="/:gameId">{productsButton}</Route>
         </Switch>
         <OverflowMenu />
       </Toolbar>
