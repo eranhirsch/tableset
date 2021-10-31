c4bf9698103fe512fc111d09d8c513c777fde63f
Eran Hirsch <eranhirsch@gmail.com>
2021-10-31 18:44:17 +0200

    megaphone

--- src/features/game/GameHome.tsx
+++ src/features/game/GameHome.tsx
@@ -1,5 +1,16 @@
-import { List, ListItem, ListItemButton, ListSubheader } from "@mui/material";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListSubheader,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useAppDispatch, useAppSelector } from "app/hooks";
+import { Vec } from "common";
+import { allProductIdsSelector } from "features/collection/collectionSlice";
 import {
   hasGameInstance,
   instanceActions,
@@ -21,11 +32,13 @@ function GameHome({ game }: { game: Readonly<Game> }): JSX.Element {
   const dispatch = useAppDispatch();
   const history = useHistory();
 
+  const products = useAppSelector(allProductIdsSelector(game));
   const hasTemplate = useAppSelector(hasGameTemplate(game));
   const hasInstance = useAppSelector(hasGameInstance(game));
 
   return (
     <>
+      {Vec.is_empty(products) && <NoProductsMegaphone game={game} />}
       <List subheader={<ListSubheader>Recipes</ListSubheader>}>
         <ListItem disableGutters>
           {hasTemplate && (
@@ -65,5 +78,29 @@ function GameHome({ game }: { game: Readonly<Game> }): JSX.Element {
         </ListItem>
       </List>
     </>
+  );
+}
+
+function NoProductsMegaphone({ game }: { game: Readonly<Game> }): JSX.Element {
+  return (
+    <Paper sx={{ paddingY: 2, paddingX: 3, marginBottom: 1 }}>
+      <Stack direction="column" spacing={1}>
+        <Typography variant="h6">
+          There are no {game.name} products in your collection.
+        </Typography>
+        <Typography variant="body2">
+          For a better experience add some <em>before</em> generating tables for
+          the game.
+        </Typography>
+        <Button
+          sx={{ width: "50%", alignSelf: "end" }}
+          size="small"
+          component={Link}
+          to="/collection"
+        >
+          Go to Collection
+        </Button>
+      </Stack>
+    </Paper>
   );
 }
