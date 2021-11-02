b55f5e461d13d1ee15b308c1c2127f5cdef41d02
Eran Hirsch <eranhirsch@gmail.com>
2021-11-02 11:25:06 +0200

    make the features nav show the active page

--- src/app/ux/FeaturesNav.tsx
+++ src/app/ux/FeaturesNav.tsx
@@ -1,11 +1,19 @@
 import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 import PeopleIcon from "@mui/icons-material/People";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function FeaturesNav(): JSX.Element {
+  const { pathname } = useLocation();
   return (
-    <BottomNavigation component="nav" showLabels value={0} sx={{ flexGrow: 0 }}>
+    <BottomNavigation
+      component="nav"
+      showLabels
+      value={
+        pathname === "/games" ? 0 : pathname === "/players" ? 1 : undefined
+      }
+      sx={{ flexGrow: 0 }}
+    >
       <BottomNavigationAction
         component={Link}
         label="Games"
