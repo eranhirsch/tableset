2dcc55fd3a754637cf5709cf4260d0610a40ebba
Eran Hirsch <eranhirsch@gmail.com>
2021-11-15 13:59:13 +0200

    missing card

--- src/games/scythe/steps/missionPossibleStep.tsx
+++ src/games/scythe/steps/missionPossibleStep.tsx
@@ -16,25 +16,35 @@ const OBJECTIVES = [
   // skip this, it's just here to make the array 1-based to match the cards
   "__ERROR",
 
+  // 1
   "Higher Ground Advantage",
   "Underworld Advantage",
   "Harvest Advantage",
   "Northern Advantage",
   "King of the Hill",
+
+  // 6
   "Send One Back as a Warning",
   "Machine Over Muscle",
   "Roll Up Your Sleeves with the Common Man",
+  "Stockpile for the Winter",
   "Woodland Advantage",
+
+  // 11
   "Population Advantage",
   "Get Rich or Cry Trying",
   "Foundations of the Empire",
   "Hedge Your Bets",
   "Balanced Workforce",
+
+  // 16
   "A Wolf Among the Sheep",
   "Divide and Conquer",
   "Become a Beloved Pacifist",
   "Shore up the Shore",
   "Create a Permanent Foothold",
+
+  // 21
   "Monopolize the Market",
   "Technological Breakthrough",
   "Achieve Tactical Mastery",
