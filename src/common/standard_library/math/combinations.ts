1b9e7c8782d66fa35f9b5878fa1b9c80933b2225
Eran Hirsch <eranhirsch@gmail.com>
2021-11-15 13:08:35 +0200

    actually no

--- src/common/standard_library/math/combinations.ts
+++ src/common/standard_library/math/combinations.ts
@@ -1,21 +1,21 @@
 import { invariant } from "common";
 import { factorial } from "./factorial";
 
-const MEMOIZE: Map<[n: number, k: number], bigint> = new Map();
+const MEMOIZE: Map<[n: number, k: number], number> = new Map();
 
-export default function combinations(n: number, k: number): bigint {
+export default function combinations(n: number, k: number): number {
   invariant(n >= 0, `Negative N ${n} are not legal`);
   invariant(k >= 0, `Negative K ${k} are not legal`);
   if (k > n) {
-    return BigInt(0);
+    return 0;
   }
   k = Math.min(k, n - k);
   let memoized = MEMOIZE.get([n, k]);
   if (memoized == null) {
     if (k === 0) {
-      memoized = BigInt(1);
+      memoized = 1;
     } else {
-      memoized = factorial(n) / (factorial(n - k) * factorial(k));
+      memoized = Number(factorial(n) / (factorial(n - k) * factorial(k)));
     }
     MEMOIZE.set([n, k], memoized);
   }
