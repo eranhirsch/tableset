9df28233b549c277ca24c1daf8a5efe468a9358e
Eran Hirsch <eranhirsch@gmail.com>
2021-11-15 13:11:02 +0200

    fix factorial

--- src/common/standard_library/math/index.ts
+++ src/common/standard_library/math/index.ts
@@ -1,7 +1,7 @@
 import combinations from "./combinations";
 import { MathUtils as combUtils } from "./combinationsLazyArray";
 import { MathUtils as containerUtils } from "./containers";
-import factorial from "./factorial";
+import { factorial } from "./factorial";
 import { MathUtils as permUtils } from "./permutationsLazyArray";
 
 export const MathUtils = {
