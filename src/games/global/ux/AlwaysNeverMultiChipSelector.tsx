308da5c2d8a86a15ad59fea28645339bf331b23d
Eran Hirsch <eranhirsch@gmail.com>
2021-11-23 19:31:33 +0200

    extracted the always never selector

---
+++ src/games/global/ux/AlwaysNeverMultiChipSelector.tsx
@@ -0,0 +1,244 @@
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import NotInterestedRoundedIcon from "@mui/icons-material/NotInterestedRounded";
+import { Box, Chip } from "@mui/material";
+import { Vec } from "common";
+import { GrammaticalList } from "games/core/ux/GrammaticalList";
+import { GamePiecesColor } from "model/GamePiecesColor";
+import React from "react";
+
+type AlwaysNeverDefinition<T> = {
+  always: readonly T[];
+  never: readonly T[];
+};
+
+type Limits = { min: number; max: number };
+
+type Mode = "always" | "never" | "random";
+
+export function AlwaysNeverMultiChipSelector<T>({
+  itemIds,
+  getLabel,
+  getColor,
+  value,
+  onChange,
+  limits,
+}: {
+  itemIds: readonly T[];
+  getLabel(itemId: T): string;
+  getColor?(itemId: T): GamePiecesColor;
+  value: Readonly<AlwaysNeverDefinition<T>>;
+  onChange(
+    changed: (
+      current: Readonly<AlwaysNeverDefinition<T>>
+    ) => Readonly<AlwaysNeverDefinition<T>>
+  ): void;
+  limits: Readonly<Limits>;
+}): JSX.Element {
+  return (
+    <Box display="flex" flexWrap="wrap" justifyContent="center" gap={1}>
+      {Vec.map(itemIds, (itemId) => (
+        <AlwaysNeverChip
+          key={`${itemId}`}
+          label={getLabel(itemId)}
+          color={getColor?.(itemId)}
+          mode={currentMode(value, itemId)}
+          onClick={() =>
+            onChange((current) =>
+              switchModes(
+                current,
+                itemId,
+                currentMode(current, itemId),
+                nextMode(current, itemId, limits, itemIds.length)
+              )
+            )
+          }
+        />
+      ))}
+    </Box>
+  );
+}
+
+function AlwaysNeverChip({
+  label,
+  mode,
+  color,
+  onClick,
+}: {
+  label: string;
+  mode: Mode;
+  color?: GamePiecesColor;
+  onClick(): void;
+}): JSX.Element {
+  return (
+    <Chip
+      sx={{
+        opacity: mode === "never" ? 0.75 : 1.0,
+        marginX:
+          mode === "random" ? "14px" : mode === "never" ? "4px" : undefined,
+      }}
+      color={color}
+      icon={
+        mode === "always" ? (
+          <CheckCircleIcon fontSize="small" />
+        ) : mode === "never" ? (
+          <NotInterestedRoundedIcon fontSize="small" />
+        ) : undefined
+      }
+      variant={mode === "never" ? "outlined" : "filled"}
+      label={
+        mode === "always" ? (
+          <strong>{label}</strong>
+        ) : mode === "never" ? (
+          <em>{label}</em>
+        ) : (
+          label
+        )
+      }
+      onClick={onClick}
+    />
+  );
+}
+
+const currentMode = <T,>(
+  { always, never }: Readonly<AlwaysNeverDefinition<T>>,
+  itemId: T
+): Mode =>
+  always.includes(itemId)
+    ? "always"
+    : never.includes(itemId)
+    ? "never"
+    : "random";
+
+function nextMode<T>(
+  config: Readonly<AlwaysNeverDefinition<T>>,
+  itemId: T,
+  { min, max }: Limits,
+  totalItems: number
+): Mode {
+  const alwaysEnabled = config.always.length < max;
+  const neverEnabled = totalItems - config.never.length > min;
+
+  switch (currentMode(config, itemId)) {
+    case "always":
+      return neverEnabled ? "never" : "random";
+    case "never":
+      return "random";
+    case "random":
+      return alwaysEnabled ? "always" : neverEnabled ? "never" : "random";
+  }
+}
+
+function switchModes<T>(
+  config: Readonly<AlwaysNeverDefinition<T>>,
+  itemId: T,
+  currentMode: Mode,
+  nextMode: Mode
+): Readonly<AlwaysNeverDefinition<T>> {
+  switch (currentMode) {
+    case "always":
+      switch (nextMode) {
+        case "always":
+          return config;
+        case "never":
+          return {
+            always: Vec.filter(config.always, (x) => x !== itemId),
+            never: Vec.sort(Vec.concat(config.never, itemId)),
+          };
+        case "random":
+          return {
+            ...config,
+            always: Vec.filter(config.always, (x) => x !== itemId),
+          };
+      }
+      break;
+
+    case "never":
+      switch (nextMode) {
+        case "always":
+          return {
+            always: Vec.sort(Vec.concat(config.always, itemId)),
+            never: Vec.filter(config.never, (x) => x !== itemId),
+          };
+        case "never":
+          return config;
+        case "random":
+          return {
+            ...config,
+            never: Vec.filter(config.never, (x) => x !== itemId),
+          };
+      }
+      break;
+
+    case "random":
+      switch (nextMode) {
+        case "always":
+          return {
+            ...config,
+            always: Vec.sort(Vec.concat(config.always, itemId)),
+          };
+        case "never":
+          return {
+            ...config,
+            never: Vec.sort(Vec.concat(config.never, itemId)),
+          };
+        case "random":
+          return config;
+      }
+  }
+}
+
+export function AlwaysNeverMultiLabel<T>({
+  value: { always, never },
+  getColor,
+  getLabel,
+}: {
+  value: AlwaysNeverDefinition<T>;
+  getColor?(itemId: T): GamePiecesColor;
+  getLabel(itemId: T): string;
+}): JSX.Element {
+  if (Vec.is_empty(always)) {
+    return (
+      <>
+        Without{" "}
+        <GrammaticalList>
+          {Vec.map(never, (itemId) => (
+            <Chip
+              key={`${itemId}`}
+              component="span"
+              color={getColor?.(itemId)}
+              size="small"
+              label={getLabel(itemId)}
+            />
+          ))}
+        </GrammaticalList>
+      </>
+    );
+  }
+
+  return (
+    <>
+      With{" "}
+      <GrammaticalList>
+        {Vec.map(always, (itemId) => (
+          <Chip
+            key={`${itemId}`}
+            component="span"
+            color={getColor?.(itemId)}
+            size="small"
+            label={getLabel(itemId)}
+          />
+        ))}
+      </GrammaticalList>
+      {!Vec.is_empty(never) && (
+        <>
+          {" "}
+          (and without{" "}
+          <GrammaticalList finalConjunction="or">
+            {React.Children.toArray(Vec.map(never, getLabel))}
+          </GrammaticalList>
+          )
+        </>
+      )}
+    </>
+  );
+}
--- src/games/scythe/steps/factionsStep.tsx
+++ src/games/scythe/steps/factionsStep.tsx
@@ -1,14 +1,10 @@
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import NotInterestedRoundedIcon from "@mui/icons-material/NotInterestedRounded";
-import { Box, Chip, Stack, Typography } from "@mui/material";
-import { useAppSelector } from "app/hooks";
+import { Chip, Stack, Typography } from "@mui/material";
 import { $, Dict, invariant, Random, Shape, Vec } from "common";
 import { InstanceCard } from "features/instance/InstanceCard";
 import {
   useHasDownstreamInstanceValue,
   useRequiredInstanceValue,
 } from "features/instance/useInstanceValue";
-import { playersSelectors } from "features/players/playersSlice";
 import { templateValue } from "features/template/templateSlice";
 import {
   ConfigPanelProps,
@@ -16,11 +12,14 @@ import {
   InstanceCardsProps,
   VariableStepInstanceComponentProps,
 } from "games/core/steps/createRandomGameStep";
-import { Query } from "games/core/steps/Query";
 import { BlockWithFootnotes } from "games/core/ux/BlockWithFootnotes";
 import { GrammaticalList } from "games/core/ux/GrammaticalList";
 import { HeaderAndSteps } from "games/core/ux/HeaderAndSteps";
 import { playersMetaStep } from "games/global";
+import {
+  AlwaysNeverMultiChipSelector,
+  AlwaysNeverMultiLabel,
+} from "games/global/ux/AlwaysNeverMultiChipSelector";
 import { PlayerId } from "model/Player";
 import React, { useMemo } from "react";
 import { ScytheProductId } from "../ScytheProductId";
@@ -34,8 +33,6 @@ type TemplateConfig = {
   always: readonly FactionId[];
   never: readonly FactionId[];
 };
-
-type Mode = "always" | "never" | "random";
 
 export default createRandomGameStep({
   id: "factions",
@@ -137,21 +134,17 @@ function ConfigPanel({
     [products]
   );
 
+  const playersCount = players.onlyResolvableValue()!.length;
+
   return (
     <Stack spacing={1} alignItems="center">
-      <FactionsSelector
-        factionIds={available}
-        factionMode={(fid) => currentMode(config, fid)}
-        onChange={(fid) =>
-          onChange((current) =>
-            switchModes(
-              current,
-              fid,
-              currentMode(current, fid),
-              nextMode(current, fid, players, available.length)
-            )
-          )
-        }
+      <AlwaysNeverMultiChipSelector
+        itemIds={available}
+        getLabel={(fid) => Factions[fid].name.full}
+        getColor={(fid) => Factions[fid].color}
+        value={config}
+        onChange={onChange}
+        limits={{ min: playersCount, max: playersCount }}
       />
       {isModular.canResolveTo(true) && (
         <Typography color="error" variant="caption">
@@ -162,126 +155,22 @@ function ConfigPanel({
   );
 }
 
-function FactionsSelector({
-  factionIds,
-  factionMode,
-  onChange,
-}: {
-  factionIds: readonly FactionId[];
-  factionMode(factionId: FactionId): Mode;
-  onChange(factionId: FactionId): void;
-}): JSX.Element {
-  return (
-    <Box display="flex" flexWrap="wrap" justifyContent="center" gap={1}>
-      {Vec.map(factionIds, (factionId) => (
-        <FactionSelector
-          key={factionId}
-          factionId={factionId}
-          mode={factionMode(factionId)}
-          onClick={() => onChange(factionId)}
-        />
-      ))}
-    </Box>
-  );
-}
-
-function FactionSelector({
-  factionId,
-  mode,
-  onClick,
-}: {
-  factionId: FactionId;
-  mode: "always" | "never" | "random";
-  onClick(): void;
-}): JSX.Element {
-  const {
-    name: { full },
-    color,
-  } = Factions[factionId];
-  return (
-    <Chip
-      sx={{
-        opacity: mode === "never" ? 0.75 : 1.0,
-        marginX:
-          mode === "random" ? "14px" : mode === "never" ? "4px" : undefined,
-      }}
-      color={color}
-      icon={
-        mode === "always" ? (
-          <CheckCircleIcon fontSize="small" />
-        ) : mode === "never" ? (
-          <NotInterestedRoundedIcon fontSize="small" />
-        ) : undefined
-      }
-      variant={mode === "never" ? "outlined" : "filled"}
-      label={
-        mode === "always" ? (
-          <strong>{full}</strong>
-        ) : mode === "never" ? (
-          <em>{full}</em>
-        ) : (
-          full
-        )
-      }
-      onClick={onClick}
-    />
-  );
-}
-
 function ConfigPanelTLDR({
   config: { always, never },
 }: {
   config: Readonly<TemplateConfig>;
 }): JSX.Element {
-  const playersCount = useAppSelector(playersSelectors.selectTotal);
-
   if (Vec.is_empty(always) && Vec.is_empty(never)) {
     // Just for consistency with other templatables
     return <>Random</>;
   }
 
-  const unassignedCount = playersCount - always.length;
-
   return (
-    <GrammaticalList>
-      {Vec.concat(
-        Vec.map_with_key(
-          Shape.select_keys(Factions, always),
-          (fid, { name: { abbreviated }, color }) => (
-            <Chip
-              key={fid}
-              component="span"
-              color={color}
-              size="small"
-              label={`${abbreviated}.`}
-            />
-          )
-        ),
-        unassignedCount > 0
-          ? [
-              <>
-                {unassignedCount} random faction
-                {unassignedCount > 1 && "s"}
-                {!Vec.is_empty(never) && (
-                  <>
-                    {" "}
-                    (but not{" "}
-                    <GrammaticalList finalConjunction="or">
-                      {React.Children.toArray(
-                        Vec.map(
-                          never,
-                          (fid) => `${Factions[fid].name.abbreviated}.`
-                        )
-                      )}
-                    </GrammaticalList>
-                    )
-                  </>
-                )}
-              </>,
-            ]
-          : []
-      )}
-    </GrammaticalList>
+    <AlwaysNeverMultiLabel
+      value={{ always, never }}
+      getLabel={(fid) => Factions[fid].name.abbreviated}
+      getColor={(fid) => Factions[fid].color}
+    />
   );
 }
 
@@ -386,96 +275,4 @@ function InstanceCards({
       )}
     </>
   );
-}
-
-const currentMode = (
-  { always, never }: Readonly<TemplateConfig>,
-  factionId: FactionId
-): Mode =>
-  always.includes(factionId)
-    ? "always"
-    : never.includes(factionId)
-    ? "never"
-    : "random";
-
-function nextMode(
-  config: Readonly<TemplateConfig>,
-  factionId: FactionId,
-  players: Query<readonly PlayerId[]>,
-  numFactions: number
-): Mode {
-  const alwaysEnabled = players.willContainNumElements({
-    min: config.always.length + 1,
-  });
-  const neverEnabled = players.willContainNumElements({
-    max: numFactions - config.never.length - 1,
-  });
-
-  switch (currentMode(config, factionId)) {
-    case "always":
-      return neverEnabled ? "never" : "random";
-    case "never":
-      return "random";
-    case "random":
-      return alwaysEnabled ? "always" : neverEnabled ? "never" : "random";
-  }
-}
-
-function switchModes(
-  config: Readonly<TemplateConfig>,
-  factionId: FactionId,
-  currentMode: Mode,
-  nextMode: Mode
-): Readonly<TemplateConfig> {
-  switch (currentMode) {
-    case "always":
-      switch (nextMode) {
-        case "always":
-          return config;
-        case "never":
-          return {
-            always: Vec.filter(config.always, (fid) => fid !== factionId),
-            never: Vec.sort(Vec.concat(config.never, factionId)),
-          };
-        case "random":
-          return {
-            ...config,
-            always: Vec.filter(config.always, (fid) => fid !== factionId),
-          };
-      }
-      break;
-
-    case "never":
-      switch (nextMode) {
-        case "always":
-          return {
-            always: Vec.sort(Vec.concat(config.always, factionId)),
-            never: Vec.filter(config.never, (fid) => fid !== factionId),
-          };
-        case "never":
-          return config;
-        case "random":
-          return {
-            ...config,
-            never: Vec.filter(config.never, (fid) => fid !== factionId),
-          };
-      }
-      break;
-
-    case "random":
-      switch (nextMode) {
-        case "always":
-          return {
-            ...config,
-            always: Vec.sort(Vec.concat(config.always, factionId)),
-          };
-        case "never":
-          return {
-            ...config,
-            never: Vec.sort(Vec.concat(config.never, factionId)),
-          };
-        case "random":
-          return config;
-      }
-  }
 }
