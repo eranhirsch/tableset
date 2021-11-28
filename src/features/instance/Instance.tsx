b2d80d37ad785d5268e29c74c91e1c3c0b00eeb5
Eran Hirsch <eranhirsch@gmail.com>
2021-11-28 13:23:37 +0200

    refactor to instance main component

--- src/features/instance/Instance.tsx
+++ src/features/instance/Instance.tsx
@@ -3,24 +3,23 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import TuneIcon from "@mui/icons-material/Tune";
 import { Box, Fab } from "@mui/material";
 import { TSPage } from "app/ux/Chrome";
-import { nullthrows, ReactUtils } from "common";
-import { GameId, GAMES } from "games/core/GAMES";
-import { useParams } from "react-router-dom";
+import { ReactUtils } from "common";
 import { AtAGlance } from "./AtAGlance";
 import { TABLE_OF_CONTENTS_PATH } from "./TableOfContents";
+import { useGameFromParam } from "./useGameFromParam";
 import { VariantSummary } from "./VariantSummary";
 
 export function Instance(): JSX.Element {
+  const navigateToParent = ReactUtils.useNavigateToParent();
   const navigateToChild = ReactUtils.useNavigateToChild();
-  const { gameId } = useParams();
 
-  const game = nullthrows(GAMES[nullthrows(gameId) as GameId]);
+  const game = useGameFromParam();
 
   return (
     <TSPage
       title={`Table for ${game.name}`}
       buttons={[
-        [<HomeIcon />, `/${gameId}`],
+        [<HomeIcon />, navigateToParent],
         [<TuneIcon />, "/template"],
       ]}
     >
