7ad71de8481d622043bc0f3407d753728a69cf9c
Eran Hirsch <eranhirsch@gmail.com>
2021-11-28 15:09:31 +0200

    bugfix

--- src/games/global/steps/createTrivialSingleItemSelector.tsx
+++ src/games/global/steps/createTrivialSingleItemSelector.tsx
@@ -172,6 +172,7 @@ function resolve<ItemId extends string | number, Pid extends ProductId>(
       ? always
       : $(
           availableForProducts(productIds!),
+          ($$) => Vec.diff($$, always),
           ($$) => Vec.diff($$, never),
           ($$) => Random.sample($$, actualCount - always.length),
           ($$) => Vec.concat(always, $$)
