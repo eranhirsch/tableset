8dcb40b7af26e86bc46aa2a9c8c11c11ba1a4e70
Eran Hirsch <eranhirsch@gmail.com>
2021-12-02 14:39:24 +0200

    fix instance steps

--- src/features/instance/InstanceStepLink.tsx
+++ src/features/instance/InstanceStepLink.tsx
@@ -1,21 +1,22 @@
 import { Link } from "@mui/material";
 import { useAppSelector } from "app/hooks";
+import { ReactUtils } from "common";
 import { gameStepSelector } from "features/game/gameSlice";
 import { StepId } from "model/Game";
 import { GameStepBase } from "model/GameStepBase";
-import { Link as RouterLink } from "react-router-dom";
 import { useInstanceActiveSteps } from "./useInstanceActiveSteps";
 
 export function InstanceStepLink(
   props: { step: GameStepBase } | { stepId: StepId }
 ): JSX.Element {
+  const navigateToSibling = ReactUtils.useNavigateToSibling();
   const activeSteps = useInstanceActiveSteps();
 
   const stepId = "stepId" in props ? props.stepId : props.step.id;
   const stepIdx = activeSteps.findIndex(({ id }) => id === stepId);
 
   return (
-    <Link component={RouterLink} to={`#${stepId}`}>
+    <Link onClick={() => navigateToSibling(stepId)}>
       Step {stepIdx + 1}: <StepLabel stepId={stepId} />
     </Link>
   );
