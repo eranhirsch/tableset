4d84d64d97c0846390ae90d39c33d33fbdf398fe
Eran Hirsch <eranhirsch@gmail.com>
2021-12-03 15:15:20 +0200

    better summary

--- src/games/scythe/steps/warAndPeaceVariant.tsx
+++ src/games/scythe/steps/warAndPeaceVariant.tsx
@@ -15,9 +15,11 @@ export default createVariant({
 
 function InstanceVariableComponent(): JSX.Element {
   return (
+    // Paraphrased from the Manual, page 51
     <Typography variant="body1">
-      Replaces the regular triumph track with either a track emphasizing
-      military force, or one discouraging battles.
+      Use the <strong>War</strong> Triumph Track if you want to encourage combat
+      between players, or use the <strong>Peace</strong> Triumph Track if you
+      want to discourage it.
     </Typography>
   );
 }
