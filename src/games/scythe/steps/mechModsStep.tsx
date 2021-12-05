69f12815dab376712cfbe98e651dbb7a73567b55
Eran Hirsch <eranhirsch@gmail.com>
2021-12-05 22:06:50 +0200

    wip

--- src/games/scythe/steps/mechModsStep.tsx
+++ src/games/scythe/steps/mechModsStep.tsx
@@ -1,3 +1,4 @@
+import { $, Vec } from "common";
 import {
   createRandomGameStep,
   VariableStepInstanceComponentProps,
@@ -5,44 +6,76 @@ import {
 import { NoConfigPanel } from "games/core/steps/NoConfigPanel";
 import { IndexHashInstanceCard } from "games/core/ux/IndexHashInstanceCards";
 import { playersMetaStep } from "games/global";
+import { FactionId } from "../utils/Factions";
+import factionsStep from "./factionsStep";
 import mechModsVariant from "./mechModsVariant";
 
-// const FACTION_ABILITIES = [
-//   // spell-checker: disable
-//   "artillery",
-//   "camaraderie",
-//   "scout",
-//   "suiton",
-//   "sword",
-//   "township",
-//   "underpass",
-// ] as const;
-// const NEW_ABILITIES = [
-//   "armor",
-//   "entrenched",
-//   "feint",
-//   "foothold",
-//   "pontoons",
-//   "regroup",
-//   "reinforce",
-//   "stealth",
-//   "tactics",
-//   // spell-checker: enable
-// ] as const;
-// const ALL_TILES = [...NEW_ABILITIES, ...FACTION_ABILITIES] as const;
-// type TileId = typeof ALL_TILES[number];
-// const TILES = Dict.merge(
-//   Dict.from_keys(FACTION_ABILITIES, () => 2),
-//   Dict.from_keys(NEW_ABILITIES, () => 3)
-// );
+const FACTION_ABILITIES = [
+  // spell-checker: disable
+  "artillery",
+  "camaraderie",
+  "scout",
+  "suiton",
+  "sword",
+  "township",
+  "underpass",
+] as const;
+
+const FACTION_ABILITY: Readonly<
+  Required<Record<typeof FACTION_ABILITIES[number], FactionId>>
+> = {
+  artillery: "nordic",
+  camaraderie: "polania",
+  scout: "crimea",
+  suiton: "togawa",
+  sword: "albion",
+  township: "rusviet",
+  underpass: "saxony",
+};
+
+const NEW_ABILITIES = [
+  "armor",
+  "entrenched",
+  "feint",
+  "foothold",
+  "pontoons",
+  "regroup",
+  "reinforce",
+  "stealth",
+  "tactics",
+  // spell-checker: enable
+] as const;
+
+const ALL_TILES = [...NEW_ABILITIES, ...FACTION_ABILITIES] as const;
+type TileId = typeof ALL_TILES[number];
+
+const TILES = $(
+  // There are 2 copies of each faction-specific ability
+  Vec.map(FACTION_ABILITIES, (ability) => Vec.fill(2, ability as TileId)),
+  ($$) =>
+    Vec.concat(
+      $$,
+      // There are 3 copies of each mech-mod only ability
+      Vec.map(NEW_ABILITIES, (ability) => Vec.fill(3, ability as TileId))
+    ),
+  Vec.flatten,
+  Vec.sort
+);
 
 export default createRandomGameStep({
   id: "mechMods",
-  dependencies: [playersMetaStep, mechModsVariant],
-  isTemplatable: (_, isEnabled) => isEnabled.canResolveTo(true)!,
+  dependencies: [playersMetaStep, mechModsVariant, factionsStep],
+
+  isTemplatable: (_, isEnabled, factions) =>
+    isEnabled.canResolveTo(true)! && factions.willResolve(),
+
   skip: (_value, [_players, isEnabled]) => !isEnabled,
 
-  resolve: (_, _players, isEnabled) => (isEnabled ? 0 : null),
+  resolve: (_, _players, isEnabled) =>
+    isEnabled
+      ? // TODO
+        0
+      : null,
 
   InstanceVariableComponent,
 
@@ -58,5 +91,6 @@ export default createRandomGameStep({
 function InstanceVariableComponent({
   value,
 }: VariableStepInstanceComponentProps<number>): JSX.Element {
+  // TODO
   return <>{value}</>;
 }
