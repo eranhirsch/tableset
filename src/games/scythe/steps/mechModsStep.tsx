9890e1d19810d84722769088a8285b3e1fe3c988
Eran Hirsch <eranhirsch@gmail.com>
2021-12-06 22:21:59 +0200

    manual mech mods

--- src/games/scythe/steps/mechModsStep.tsx
+++ src/games/scythe/steps/mechModsStep.tsx
@@ -11,6 +11,7 @@ import {
 } from "games/core/steps/createRandomGameStep";
 import { NoConfigPanel } from "games/core/steps/NoConfigPanel";
 import { GrammaticalList } from "games/core/ux/GrammaticalList";
+import { HeaderAndSteps } from "games/core/ux/HeaderAndSteps";
 import { IndexHashCaption } from "games/core/ux/IndexHashCaption";
 import { IndexHashInstanceCard } from "games/core/ux/IndexHashInstanceCards";
 import { RulesSection } from "games/global/ux/RulesSection";
@@ -36,7 +37,7 @@ export default createRandomGameStep({
     isEnabled && factionIds != null ? MechMods.randomIdx(factionIds) : null,
 
   InstanceVariableComponent,
-  // TODO: InstanceManualComponent
+  InstanceManualComponent,
 
   instanceAvroType: "long",
 
@@ -94,30 +95,73 @@ function InstanceVariableComponent({
         )}
       </Stack>
       <IndexHashCaption idx={mechModsIdx} />
-      <RulesSection>
-        <>
-          Certain Mech Mods have a symbol to indicate they cannot be used
-          against non-player units.
-        </>
-        <>After a game begins, you may not reorganize your Mods.</>
-        <strong>MECH MOD CLARIFICATIONS:</strong>
-        <>
-          <em>ARMOR:</em> The attacker decides which card to discard.
-        </>
-        <>
-          <em>FEINT:</em> After adjusting your combat dial, you must be able to
-          pay the new Power total. You do not pay the original amount.
-        </>
-        <>
-          <em>STEALTH:</em> If you use Stealth and another ability (Speed,
-          Factory card, etc) to move through a territory where you would
-          normally initiate combat, force workers to retreat, or trigger a token
-          penalty, you completely ignore everything on that territory.
-        </>
-        <>
-          <em>TACTICS:</em> Limited to once per combat.
-        </>
-      </RulesSection>
+      <Rules />
     </>
+  );
+}
+
+function InstanceManualComponent(): JSX.Element {
+  return (
+    <>
+      <HeaderAndSteps synopsis="Each player:">
+        <>
+          Randomly draws <strong>4</strong> Mech Mods from the general supply.
+          Players all draw, then make their choices simultaneously.
+        </>
+        <>
+          Chooses{" "}
+          <strong>
+            1, 2, <em>or none</em>
+          </strong>{" "}
+          of them.
+        </>
+        <>
+          Places them <strong>over</strong> any of their faction's printed mech
+          abilities.
+        </>
+        <>Discard the rest.</>
+      </HeaderAndSteps>
+      <Rules withSelectionRule />
+    </>
+  );
+}
+
+function Rules({
+  withSelectionRule = false,
+}: {
+  withSelectionRule?: boolean;
+}): JSX.Element {
+  return (
+    <RulesSection>
+      {withSelectionRule && (
+        <>
+          When drawing Mech Mods, discard any duplicates (e.g., from your draw,
+          Mods you already have, and abilities on your current faction mat) and
+          draw replacements. You may only have 1 of each Mod.
+        </>
+      )}
+      <>
+        Certain Mech Mods have a symbol to indicate they cannot be used against
+        non-player units.
+      </>
+      <>After a game begins, you may not reorganize your Mods.</>
+      <strong>MECH MOD CLARIFICATIONS:</strong>
+      <>
+        <em>ARMOR:</em> The attacker decides which card to discard.
+      </>
+      <>
+        <em>FEINT:</em> After adjusting your combat dial, you must be able to
+        pay the new Power total. You do not pay the original amount.
+      </>
+      <>
+        <em>STEALTH:</em> If you use Stealth and another ability (Speed, Factory
+        card, etc) to move through a territory where you would normally initiate
+        combat, force workers to retreat, or trigger a token penalty, you
+        completely ignore everything on that territory.
+      </>
+      <>
+        <em>TACTICS:</em> Limited to once per combat.
+      </>
+    </RulesSection>
   );
 }
