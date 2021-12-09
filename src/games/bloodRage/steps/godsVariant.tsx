ef3b6b1abfa6f57a7b36cde16cc58487e220e81b
Eran Hirsch <eranhirsch@gmail.com>
2021-12-09 19:04:24 +0200

    shorten description

--- src/games/bloodRage/steps/godsVariant.tsx
+++ src/games/bloodRage/steps/godsVariant.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { createVariant } from "games/core/steps/createVariant";
 import productsMetaStep from "./productsMetaStep";
 
@@ -6,7 +7,16 @@ export default createVariant({
   name: "Gods",
   dependencies: [productsMetaStep],
   isTemplatable: (products) => products.willContain("gods")!,
-  Description:
-    // Copied from BoardGameGeek product description
-    "creates a unique texture to that session. They affect draft choices and influence the flow of the battles. Will you try to benefit from a god's power again and again by moving him where you want and always pillaging his province? Or will you do your best to neutralize him by pillaging around his province? Perhaps you'll want to attempt a hopeless pillage on his province just to move him onto another province where he will hinder another player's plans. They add a whole new layer to the game, without adding hardly any complexity to the rules",
+  Description,
 });
+
+function Description(): JSX.Element {
+  return (
+    <Typography variant="body1">
+      {/* Copied from BoardGameGeek product description */}
+      Creates a unique texture to the session. They affect draft choices and
+      influence the flow of the battles. They add a whole new layer to the game,
+      without adding hardly any complexity to the rules.
+    </Typography>
+  );
+}
