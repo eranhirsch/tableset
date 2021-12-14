0f72ebb24a0f777d3cb862b518eefeb4ba17d8bc
Eran Hirsch <eranhirsch@gmail.com>
2021-12-14 12:26:41 +0200

    unimplemented

--- src/games/wingspan/wingspanGame.ts
+++ src/games/wingspan/wingspanGame.ts
@@ -6,10 +6,31 @@ export default createGame({
   name: "Wingspan",
   productsMetaStep,
   products: {
-    base: { name: "Wingspan", bggId: 266192, isBase: true, year: 2019 },
-    swiftStart: { name: "Swift-Start Promo Pack", bggId: 290837, year: 2019 },
-    europe: { name: "European Expansion", bggId: 290448, year: 2019 },
-    oceania: { name: "Oceania Expansion", bggId: 300580, year: 2020 },
+    base: {
+      name: "Wingspan",
+      bggId: 266192,
+      isBase: true,
+      year: 2019,
+      isNotImplemented: true,
+    },
+    swiftStart: {
+      name: "Swift-Start Promo Pack",
+      bggId: 290837,
+      year: 2019,
+      isNotImplemented: true,
+    },
+    europe: {
+      name: "European Expansion",
+      bggId: 290448,
+      year: 2019,
+      isNotImplemented: true,
+    },
+    oceania: {
+      name: "Oceania Expansion",
+      bggId: 300580,
+      year: 2020,
+      isNotImplemented: true,
+    },
   },
   steps: [],
 });
