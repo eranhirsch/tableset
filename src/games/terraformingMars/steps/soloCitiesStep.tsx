c655763d30c02425fd5fbf8f955f2dabc5c2644d
Eran Hirsch <eranhirsch@gmail.com>
2021-12-26 15:16:30 +0200

    solo cities

--- src/games/terraformingMars/steps/soloCitiesStep.tsx
+++ src/games/terraformingMars/steps/soloCitiesStep.tsx
@@ -3,7 +3,10 @@ import {
   createDerivedGameStep,
   DerivedStepInstanceComponentProps,
 } from "games/core/steps/createDerivedGameStep";
+import { ChosenElement } from "games/core/ux/ChosenElement";
+import { HeaderAndSteps } from "games/core/ux/HeaderAndSteps";
 import { playersMetaStep } from "games/global";
+import { RulesSection } from "games/global/ux/RulesSection";
 
 export default createDerivedGameStep({
   id: "soloCities",
@@ -16,5 +19,51 @@ export default createDerivedGameStep({
 function InstanceDerivedComponent({
   dependencies: [_playerIds],
 }: DerivedStepInstanceComponentProps<readonly PlayerId[]>): JSX.Element {
-  return <>TODO</>;
+  return (
+    <>
+      <HeaderAndSteps
+        synopsis={
+          <>
+            Place <strong>2</strong>{" "}
+            <ChosenElement extraInfo="tiles">neutral city</ChosenElement> on the
+            map with an{" "}
+            <ChosenElement extraInfo="tile">adjacent greenery</ChosenElement>{" "}
+            each:
+          </>
+        }
+      >
+        <>
+          Reveal and discard the <strong>4</strong> top cards of the deck.{" "}
+          <em>
+            Use their cost numbers to determine the positions of the tiles
+          </em>
+          .
+        </>
+        <>
+          The <ChosenElement>first city</ChosenElement> is placed counting from{" "}
+          <strong>top left to right and down</strong>, like reading.{" "}
+          <em>Skip any illegal placements (like areas reserved for ocean)</em>.
+        </>
+        <>
+          For the <ChosenElement>second city</ChosenElement> you step backwards
+          from <strong>bottom right</strong> in the same fashion.
+        </>
+        <>
+          Place the two greeneries by counting the cost numbers and stepping{" "}
+          <strong>clockwise around each city</strong>, starting from{" "}
+          <em>top left</em>, <em>skipping illegal placements</em>.
+        </>
+      </HeaderAndSteps>
+      <RulesSection>
+        <>These tiles are not yours, and do not increase the oxygen level.</>
+        <>
+          <em>
+            If you choose to play <strong>Tharsis Republic</strong> this game
+          </em>
+          : you get M€ production for the 2 neutral cities even though they are
+          placed before you reveal your corporation.
+        </>
+      </RulesSection>
+    </>
+  );
 }
