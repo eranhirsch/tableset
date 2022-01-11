b9ac955657d2408b05d6a1e46a2fbe010add4a6b
Eran Hirsch <eranhirsch@gmail.com>
2022-01-11 11:32:52 +0200

    card numbers

--- src/games/wingspan/steps/birdCardsStep.tsx
+++ src/games/wingspan/steps/birdCardsStep.tsx
@@ -1,5 +1,8 @@
 import { createGameStep } from "games/core/steps/createGameStep";
+import { ChosenElement } from "games/core/ux/ChosenElement";
 import { HeaderAndSteps } from "games/core/ux/HeaderAndSteps";
+
+const NUM_CARDS = 170;
 
 export default createGameStep({
   id: "birdCards",
@@ -9,11 +12,15 @@ export default createGameStep({
 function InstanceManualComponent(): JSX.Element {
   return (
     <HeaderAndSteps>
-      {/* TODO: Add a count of cards based on expansions. */}
-      <>Shuffle the bird cards into a deck.</>
-      <>Place it next to the bird tray.</>
-      {/* TODO: It's overkill but we technically can randomize */}
       <>
+        Shuffle the <strong>{NUM_CARDS}</strong>{" "}
+        <ChosenElement extraInfo="cards">Bird</ChosenElement> into a deck.
+      </>
+      <>
+        Place it next to the <em>bird tray</em>.
+      </>
+      <>
+        {/* TODO: It's overkill but we technically can randomize */}
         Populate it with <strong>3</strong> face-up bird cards.
       </>
     </HeaderAndSteps>
