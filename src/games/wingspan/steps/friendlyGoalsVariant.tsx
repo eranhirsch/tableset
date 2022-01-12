d6bafbc827f25ec7c3844f0d00b38a9837c3819c
Eran Hirsch <eranhirsch@gmail.com>
2022-01-12 12:24:06 +0200

    disable friendly when swift is used

--- src/games/wingspan/steps/friendlyGoalsVariant.tsx
+++ src/games/wingspan/steps/friendlyGoalsVariant.tsx
@@ -1,10 +1,12 @@
 import { createVariant } from "games/core/steps/createVariant";
+import swiftStartVariant from "./swiftStartVariant";
 
 export default createVariant({
   id: "friendlyGoals",
   name: "Friendly Goals",
   dependencies: [],
   isTemplatable: () => true,
+  incompatibleWith: swiftStartVariant,
   Description:
     "For a game with less direct competition between players. This is good for new players.",
 });
